a9c4850a8bc3e0cb63b767895dbbaaca015745b2
sixLUKEY <marcoslukerojo@gmail.com>
2025-09-06 21:15:24 +0200

    [#2] - implemented cli entrypoint while also initialising npm workspace

---
+++ cmd/parsemd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: parsemd parse <file>")
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+	filepath := os.Args[2]
+
+	switch command {
+	case "parse", "p":
+		parse(filepath)
+	default: 
+		fmt.Printf("Unknown command: %s\n", command)
+		os.Exit(1)
+	}
+}
+
+func parse(filePath string) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(string(data))
+}
